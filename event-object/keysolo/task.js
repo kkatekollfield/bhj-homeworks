d4f87d92567b268840e4909f71de6af9366b0895
Kate Kollfield <katekollfield@gmail.com>
2026-08-11 22:03:01 +0300

    first commit keysolo

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -4,6 +4,10 @@ class Game {
     this.wordElement = container.querySelector('.word');
     this.winsElement = container.querySelector('.status__wins');
     this.lossElement = container.querySelector('.status__loss');
+    this.timerElement = container.querySelector('.status__timer');
+    this.timerInterval = null;
+    this.wordLength = 0;
+    this.isGameStarted = false;
 
     this.reset();
 
@@ -11,21 +15,72 @@ class Game {
   }
 
   reset() {
+    this.stopTimer();
+    this.isGameStarted = false;
+    this.timerElement.classList.remove('status__timer_danger');
     this.setNewWord();
     this.winsElement.textContent = 0;
     this.lossElement.textContent = 0;
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода символа вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+    document.addEventListener('keydown', (event) => {
+      if (!this.isGameStarted && event.key.length === 1 && !event.ctrlKey && !event.altKey) {
+                this.isGameStarted = true;
+                this.startTimer(this.wordElement.textContent.length);
+            }
+
+      if (event.key.length === 1 && !event.ctrlKey && !event.altKey) {
+        const currentChar = this.currentSymbol.textContent.toLowerCase();
+        const typedChar = event.key.toLowerCase();
+
+        if (currentChar === typedChar) {
+          this.success();
+        } else {
+          this.fail();
+        } 
+      }
+  });
   }
+
+  startTimer(seconds) {
+    this.timerElement.classList.remove('status__timer_danger');
+    this.wordLength = seconds;
+
+    if (this.timerInterval) {
+      clearInterval(this.timerInterval);
+      this.timerInterval = null;
+    }
+
+    this.timeLeft = seconds;
+    this.timerElement.textContent = this.timeLeft;
+
+    this.timerInterval = setInterval(() => {
+      this.timeLeft--;
+      this.timerElement.textContent = this.timeLeft;
+      
+      if(this.wordLength > 3) {
+        if (this.timeLeft <= 3) {
+          this.timerElement.classList.add('status__timer_danger');
+        } else {
+          this.timerElement.classList.remove('status__timer_danger');
+        }
+      }
+      
+      if (this.timeLeft <= 0) {
+        clearInterval(this.timerInterval);
+        this.timerInterval = null;
+        this.fail();
+      }
+    }, 1000);
+  }
+
+stopTimer() {
+    if (this.timerInterval) {
+        clearInterval(this.timerInterval);
+        this.timerInterval = null;
+    }
+}
 
   success() {
     if(this.currentSymbol.classList.contains("symbol_current")) this.currentSymbol.classList.remove("symbol_current");
@@ -37,6 +92,8 @@ class Game {
       return;
     }
 
+    this.stopTimer();
+
     if (++this.winsElement.textContent === 10) {
       alert('Победа!');
       this.reset();
@@ -45,7 +102,8 @@ class Game {
   }
 
   fail() {
-    if (++this.lossElement.textContent === 5) {
+    this.stopTimer();
+    if (++this.lossElement.textContent === 3) {
       alert('Вы проиграли!');
       this.reset();
     }
@@ -53,9 +111,15 @@ class Game {
   }
 
   setNewWord() {
+    this.timerElement.classList.remove('status__timer_danger');
     const word = this.getWord();
-
     this.renderWord(word);
+
+    if (this.isGameStarted) {
+        this.startTimer(word.length);
+    } else {
+        this.timerElement.textContent = 0;
+    }
   }
 
   getWord() {
