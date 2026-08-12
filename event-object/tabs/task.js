d0d4d7a24cc771173fab359b4ea9952fe8677f99
Kate Kollfield <katekollfield@gmail.com>
2026-08-12 16:03:26 +0300

    first commit tabs

---
+++ event-object/tabs/task.js
@@ -0,0 +1,16 @@
+const tabsContainers = document.querySelectorAll('.tabs')
+
+tabsContainers.forEach((container) => {
+    const tabs = container.querySelectorAll('.tab')
+    const contents = container.querySelectorAll('.tab__content')
+
+    tabs.forEach((tab, index) => {
+        tab.addEventListener('click', () => {
+            tabs.forEach(t => t.classList.remove('tab_active'));
+            tab.classList.add('tab_active');
+
+            contents.forEach(c => c.classList.remove('tab__content_active'));
+            contents[index].classList.add('tab__content_active');
+        });
+    });
+});
