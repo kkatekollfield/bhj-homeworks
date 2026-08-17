4039ab815b9b424a44b3aa17d7f7ea538d78e423
Kate Kollfield <katekollfield@gmail.com>
2026-08-17 20:42:58 +0300

    refactored condition with one variable

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,14 +1,27 @@
 const blocksRevealed = document.querySelectorAll('.reveal')
-var isInViewport = function(element) {
-    const rect = element.getBoundingClientRect();
-    const viewportHeight = window.innerHeight;
+// var isInViewport = function(element) {
+//     const rect = element.getBoundingClientRect();
+//     const viewportHeight = window.innerHeight;
     
-    return rect.top < viewportHeight && rect.bottom > 0;
-};
+//     return rect.top < viewportHeight && rect.bottom > 0;
+// };
+
+// window.addEventListener('scroll', () => {
+//     blocksRevealed.forEach(element => {
+//         if (isInViewport(element)) {
+//             element.classList.add('reveal_active');
+//         } else {
+//             element.classList.remove('reveal_active');
+//         }
+//     });
+// });
 
 window.addEventListener('scroll', () => {
     blocksRevealed.forEach(element => {
-        if (isInViewport(element)) {
+        const {innerHeight} = window;
+        const {top} = element.getBoundingClientRect();
+
+        if (top < innerHeight && top > 0) {
             element.classList.add('reveal_active');
         } else {
             element.classList.remove('reveal_active');
